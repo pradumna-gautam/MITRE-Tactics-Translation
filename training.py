d827ef49a3b9d6dd1317412c7cf779dbf1af76de
pradumna-gautam <pradumna-gautam@pgautam8601@gmail.com>
2023-03-28 19:06:28 -0400

    Saving and Loading Fine-Tuned Model

--- training.py
+++ training.py
@@ -252,3 +252,27 @@ rep = classification_report(flat_true_labels, flat_predictions, target_names =['
 print('Total Acc: %.3f' % acc)
 print(rep)
 #print(cab)
+
+
+## **Saving and Loading Fine-Tuned Model**
+
+import os
+
+# Saving best-practices: if you use defaults names for the model, you can reload it using from_pretrained()
+
+output_dir = './model_save/'
+
+# Create output directory if needed
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+print("Saving model to %s" % output_dir)
+
+# Save a trained model, configuration and tokenizer using `save_pretrained()`.
+# They can then be reloaded using `from_pretrained()`
+model_to_save = model.module if hasattr(model, 'module') else model  # Take care of distributed/parallel training
+model_to_save.save_pretrained(output_dir)
+tokenizer.save_pretrained(output_dir)
+
+# Good practice: save your training arguments together with the trained model
+# torch.save(args, os.path.join(output_dir, 'training_args.bin'))
